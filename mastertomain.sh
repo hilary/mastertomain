c845e51a4cbca662bc97a5c35bde88943ae7536b
Hilary Holz <hholz@digitalocean.com>
2020-06-26 10:56:32 -0700

    let user change to a branch name other than main

--- mastertomain.sh
+++ mastertomain.sh
@@ -3,10 +3,10 @@
 set -eu
 
 function create_main {
-  echo "creating main..."
+  echo "creating ${MAIN}..."
   git checkout master
   git pull origin master
-  git checkout -b main  
+  git checkout -b "${MAIN}"  
   echo "done"
   echo ""
 }
@@ -28,7 +28,9 @@ else
   exit 1
 fi
 
-echo "Welcome! I will help you convert your master branch to be called main."
+MAIN=${MAIN:-main}
+
+echo "Welcome! I will help you convert your master branch to be called ${MAIN}."
 echo "Black Lives Matter!"
 echo "Some steps are automated, others you will need to do manually. I'll prompt you for each manual step,"
 echo "after which you can come back here and continue."
@@ -40,35 +42,33 @@ create_main
 if find_master; then
   echo "I found some uses of the word 'master' in your code. I'm going to present them to you one at a time"
   echo "for you to review and accept or deny. Please review them carefully, because I will commit the changes"
-  echo "you accept."
+  echo "you accept. Hit enter to continue"
   echo ""
   for filename in $(find . -type f -name "*" ! -path './.git/*' ! -name 'mastertomain.sh' ! -path './vendor/*'); do 
-    vim -c "%s/master/main/gc" -c "wq" "${filename}"; 
+    vim -c "%s/master/${MAIN}/gc" -c "wq" "${filename}"; 
   done
   echo ""
   echo "Phew! All done!"
   echo ""
   echo "Committing changes..."
   git add .
-  git commit -m "convert master to main"
+  git commit -m "convert master to ${MAIN}"
   echo "done"
   echo ""
 fi
-echo "Pushing changes to github..."
-git push --set-upstream origin main
+echo "Pushing ${MAIN} to github..."
+git push --set-upstream origin "${MAIN}"
 echo "done"
 echo ""
 
 echo "Now I'm going to walk you through a series of manual steps, after I'll do a last few scripted steps."
 echo ""
-echo "I have opened a pull request for 'main’."
-echo "Go to your repo on GitHub and merge the changes. Hit enter to continue"
-read -r
-echo "Still on Github, under settings => branches, update any branch protections. Hit enter to continue."
+
+echo "Go to your repo on GitHub. Under settings => branches, update any branch protections. Hit enter to continue."
 read -r
 echo "Update any *external* dependencies, e.g, concourse. Hit enter to continue."
 read -r
-echo "Back on Github under settings => branches, update the default branch to 'main'. Hit enter to continue."
+echo "Back on Github under settings => branches, update the default branch to ${MAIN}. Hit enter to continue."
 read -r
 
 echo "Deleting master (drumroll please!)..."
